cd1e53476e376f283594736e9d20698bd62b4aa1
Jacob Cunningham <jcun4128@gmail.com>
2023-06-03 20:08:39 -0500

    layout, data mapped, adding manual connection button

--- app/bluetooth/bluetooth.py
+++ app/bluetooth/bluetooth.py
@@ -21,7 +21,7 @@ from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
 
 class Bluetooth():
-  def __init__(self):
+  def __init__(self, monocle_not_found, run_app):
     self.UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
     self.UART_RX_CHAR_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
     self.UART_TX_CHAR_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
@@ -31,6 +31,9 @@ class Bluetooth():
     self.res = None
     self.disconnect = False
     self.send_bytes_data = None # eg. b'string'
+    self.monocle_not_found = monocle_not_found
+    self.first_conn_failed = False
+    self.run_app = run_app
 
   async def repl_api(self):
     def match_nus_uuid(device: BLEDevice, adv: AdvertisementData):
@@ -45,20 +48,23 @@ class Bluetooth():
     device = await BleakScanner.find_device_by_filter(match_nus_uuid)
 
     if device is None:
-        print("no matching device found, you may need to edit match_nus_uuid().")
-        sys.exit(1)
+      print("no matching device found, you may need to edit match_nus_uuid().")
+      self.first_conn_failed = True
+      self.monocle_not_found()
+      time.sleep(1)
+      await self.repl_api() # keep trying to connect
+    else:
+      def handle_disconnect(_: BleakClient):
+        print("Device was disconnected, goodbye.")
+        # cancelling all tasks effectively ends the program
+        for task in asyncio.all_tasks():
+          task.cancel()
 
-    def handle_disconnect(_: BleakClient):
-      print("Device was disconnected, goodbye.")
-      # cancelling all tasks effectively ends the program
-      for task in asyncio.all_tasks():
-        task.cancel()
+      def handle_rx(_: BleakGATTCharacteristic, data: bytearray):
+        self.res = data
+        print("received:", data)
 
-    def handle_rx(_: BleakGATTCharacteristic, data: bytearray):
-      self.res = data
-      print("received:", data)
-
-    async with BleakClient(device, disconnected_callback=handle_disconnect) as client:
+      async with BleakClient(device, disconnected_callback=handle_disconnect) as client:
         await client.start_notify(self.UART_TX_CHAR_UUID, handle_rx)
 
         self.connected = True
@@ -71,6 +77,11 @@ class Bluetooth():
             await client.write_gatt_char(self.rx_char, self.send_bytes_data)
             await asyncio.sleep(0.5)
             self.send_bytes_data = None
+
+          if (self.first_conn_failed):
+            self.first_conn_failed = False
+            self.run_app()
+
           time.sleep(1) # slow this loop down, does mean longer response time
 
   def start(self):
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # https://www.tutorialspoint.com/converting-tkinter-program-to-exe-file
 # https://www.tutorialspoint.com/python/tk_labelframe.htm
 
+import json
 import time
 
 from threading import Thread
@@ -10,6 +11,7 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 
 ble = None
+stop_app = False
 
 win = Tk()
 win.title('Monocle Dock')
@@ -25,42 +27,81 @@ img = ImageTk.PhotoImage(img)
 canvas.create_image(50, 50, anchor=NW, image=img)
 
 #Creates a Frame
-frame = LabelFrame(win, width= 300, height= 50, bd=1, text='Monocle status:', bg='#282828', fg='#AAFF00')
+frame = LabelFrame(win, width= 300, height= 50, bd=1, text='Monocle status:', bg='#282828', fg='#0096FF')
 frame.pack(fill='both', expand='yes')
 frame.pack_propagate() # False keeps width
 
-status_text = Label(frame, text='Connecting...', anchor='sw')
-status_text.place(relx=0, rely=50, anchor='sw') # lol NW
-status_text.pack()
+connected_text = Label(frame, text='Connecting...', bg='#282828', fg='#AAFF00')
+connected_text.place(x=0, y=0)
+
+charging_text = Label(frame, text="Charging:", bg='#282828', fg='#AAFF00')
+charging_text.place(x=0, y=20)
+
+firmware_text = Label(frame, text="Firmware:", bg='#282828', fg='#AAFF00', anchor="e")
+firmware_text.place(x=150, y=0)
+
+ram_text = Label(frame, text="RAM:", bg='#282828', fg='#AAFF00', anchor="e")
+ram_text.place(x=150, y=20)
+
+storage_text = Label(frame, text="Storage:", bg='#282828', fg='#AAFF00', anchor="e")
+storage_text.place(x=150, y=40)
 
 def clean_res(res_str):
   return res_str.replace('OK', '').replace('\x04', '').replace('>', '')
 
-def app_logic():
+def update_status():
+  ble.send_bytes_data = b'print(get_monocle_status())\x04' # this is a function flashed on monocle eg. main.py see monocle_main.py
+  time.sleep(2)
+  
+  status = json.loads(clean_res(ble.res.decode('utf-8')))
+
+  firmware_text.config(text = "Firmware: " + status['firmware'])
+  ram_text.config(text = "RAM: " + str(status['ram']) + ' kb')
+  storage_text.config(text = "Storage: " + str(status['storage']) + ' kb')
+  charging_text.config(text = "Charging: " + ('yes' if status['charging'] else 'no'))
+
+def monocle_not_found():
+  connected_text.config(text = 'Searching for monocole...')
+  win.update()
+
+def run_app():
   global ble
 
-  ble = Bluetooth()
+  if (ble.connected):
+    connected_text.config(text = 'Connected')
+    ble.send_bytes_data = b'\x03\x01'
+    time.sleep(2)
+    ble.send_bytes_data = b'import device\x04' # have to reimport this for some reason
+    time.sleep(2)
+    update_status()
+    win.update()
+
+    time.sleep(5)
+
+    while (stop_app != True):
+      update_status()
+      win.update()
+      time.sleep(5)
+
+def start_ble():
+  global ble
+
+  ble = Bluetooth(monocle_not_found, run_app)
   Thread(target=ble.start).start()
   
   time.sleep(3)
-
-  if (ble.connected):
-    ble.send_bytes_data = b'\x03\x01'
-    time.sleep(2)
-    ble.send_bytes_data = b'import device;print(device.VERSION)\x04'
-    time.sleep(2)
-    status_text.config(text = clean_res(ble.res.decode('utf-8')))
-    win.update()
-    print('sent all')
+  run_app()
 
 # handle closing Monocle Dock
 def on_close():
-  global ble
+  global ble, stop_app
 
   ble.disconnect = True # stops connection loop
+  stop_app = True
   win.destroy()
 
-win.after(1000, app_logic) # run app logic after desktop app renders
+Thread(target=start_ble).start()
+
 win.protocol("WM_DELETE_WINDOW", on_close)
 
 # render UI

68d223b11f8304b37585f3912c645a1a1f0a4e8d
Jacob Cunningham <jcun4128@gmail.com>
2023-07-05 21:12:24 -0500

    not receiving

--- README.md
+++ README.md
@@ -1,41 +1,5 @@
 ### About
 
-<img src="./demo.gif"/>
+This produces a wav file to be played on host OS eg. win 10 or whatever you write code with.
 
-This desktop app is for my main computer. It is paired with the magnetic usb charging below with the pemeranent [clip mod](https://github.com/jdc-cunningham/bl-monocle-stuff/tree/master/3d-printing/glasses-permanent-clip).
-
-Primarily I am interested in whether the device is charging or not but I have also included other data.
-
-### Made with Python Bleak and Tkinter
-
-I have wrapped the [uart_service.py](https://github.com/hbldh/bleak/blob/develop/examples/uart_service.py) example from Bleak and I'm using it as a polling thread to pick up commands sent to the Monocle.
-
-### Requires `main.py` on Monocle
-
-For this code to work, you need [this code](monocle_main.py) flashed onto the Monocle. It provides a helper function that grabs all the different statuses eg. battery level to display on the dock.
-
-### App flow
-
-There are two main files:
-- [main.py](app/main.py)
-- [bluetooth.py](app/bluetooth/bluetooth.py)
-
-In `main.py` the desktop UI (dock) is built up then the app logic `start_ble` is started as a thread. In `start_ble` the connection/listener loop thread for the monocle bluetooth is started. Then the app logic `run_app`starts. At this point the dock has rendered and the monocle is either connected or it isn't. The dock will continue to try and connect to the monocle (since monocle can only be connected to one thing at a time). The connect/disconnect button is there for manual control.
-
-Closing the dock app (top-right X) should stop any related threads that were spawned by this app.
-
-### Making an executable
-
-This uses [pyinstaller](https://www.tutorialspoint.com/converting-tkinter-program-to-exe-file) to make an exe. Otherwise you can just run the app using `$python main.py`.
-
-`$python -m PyInstaller -F main.py --name=MonocleDock --windowed`
-
-Note: the paths of the images have to be set in `MonocleDock.spec` which is produced the first time you run the above file
-
-### Issues
-
-- Monocle may not connect on first try, this is supposed to be handled by a retry in `bluetooth.py`eg. `first_conn_failed` but so far that is not working
-
-### Disclaimer
-
-This does not consider large text being sent to the Monocle eg. entire scripts.
+It's just a way to test if the mic is working.
--- app/main.py
+++ app/main.py
@@ -1,101 +1,58 @@
-# https://www.tutorialspoint.com/converting-tkinter-program-to-exe-file
-# https://www.tutorialspoint.com/python/tk_labelframe.htm
-
-import os
-import json
 import time
-import math
+import wave
+import numpy as np
 
-from threading import Thread
 from bluetooth.bluetooth import Bluetooth
-from tkinter import *
-from tkinter import ttk
-from PIL import Image, ImageTk
+from threading import Thread
 
+loop_counter = 0
+res_counter = 0 # lazy way to avoid first response from raw mode
 ble = None
-stop_app = False
 
-# https://stackoverflow.com/a/51266275/2710227
-def resource_path(relative_path):
-  try:
-    base_path = sys._MEIPASS
-  except Exception:
-    base_path = os.path.abspath(".")
-
-  return os.path.join(base_path, relative_path)
-
-def clean_res(res_str):
-  return res_str.replace('OK', '').replace('\x04', '').replace('>', '')
-
-# this clock is not that accurate, there is some overlap
-# with the 3s incrementer below and the 1 - 1.5 sec loop in bluetooth.py
-def format_time(seconds):
-  if (seconds < 60):
-    return str(seconds) + 's'
-  elif (seconds >= 60 and seconds < 3600):
-    mins = math.floor(seconds / 60)
-    seconds = math.floor(seconds % 60)
-
-    return str(mins) + 'm ' + str(seconds) + 's'
-  else:
-    hours = math.floor(seconds / 3600)
-    mins = math.floor((seconds - (hours * 3600))/60)
-    seconds = seconds - ((hours * 3600) + (mins * 60))
-
-    return str(hours) + 'h ' + str(mins) + 'm ' + str(seconds) + 's'
-
-def update_status():
-  ble.send_bytes_data = b'print(get_monocle_status())\x04' # this is a function flashed on monocle eg. main.py see monocle_main.py
-  time.sleep(2)
-
-  status = {}
-  
-  if (ble.res == None):
-    print('no data from monocle')
-    status = {
-      "charging": False,
-      "ram": 0,
-      "storage": 0,
-      "firmware": 'v0.0.0',
-      "batt": 0,
-      "uptime": 0
-    }
-  else:
-    status = json.loads(clean_res(ble.res.decode('utf-8')))
-
-  ble.res = None # reset
-
-  firmware_text.config(text="Firmware: " + status['firmware'])
-  # https://stackoverflow.com/a/45367591/2710227
-  ram_text.config(text="RAM: " + str(f"{status['ram']:,}") + ' b')
-  storage_text.config(text="Storage: " + str(f"{status['storage']:,}") + ' b')
-  charging_text.config(text="Charging: " + ('yes' if status['charging'] else 'no'), fg=('#AAFF00' if status['charging'] else '#D22B2B'))
-  batt_level_text.config(text="Battery: " + str(status['batt']) + " %")
-  uptime_text.config(text="Uptime: " + format_time(status['uptime']))
+# https://stackoverflow.com/a/75026723/2710227
+num_channels    = 1
+sample_width    = 8
+sample_rate     = 8000
+frequency       = 440
+duration        = 4
 
 def monocle_not_found():
-  connected_text.config(text = 'Searching for monocole...')
+  print('searching for monocle...')
 
 def run_app():
-  global ble
+  global ble, loop_counter, res_counter
 
   if (ble.connected):
-    connected_text.config(text='Connected', fg='#AAFF00')
-    connection_button.config(text='Disconnect')
+    print('monocle connected')
+    ble.send_bytes_data = b'\x03\x01' # raw mode
 
-    ble.send_bytes_data = b'\x03\x01'
-    time.sleep(2)
+  while True:
+    loop_counter += 1
 
-    ble.send_bytes_data = b'import device\x04' # have to reimport this for some reason
-    time.sleep(2)
+    if (ble.res != None and res_counter == 0):
+      res_counter += 1
+      continue
 
-    update_status()
+    if (ble.res != None or res_counter != 0):
+      print('data received')
 
-    time.sleep(5)
+      n = round(sample_rate/frequency)
+      t = np.linspace(0, 1/frequency, n)
+      data = (127*np.sin(2*np.pi*frequency*t)).astype(np.int8)
+      periods = round(frequency*duration)
 
-    while (stop_app != True or ble.connected != False):
-      update_status()
-      time.sleep(10)
+      with wave.open('test.wav', 'w') as wavfile:
+        wavfile.setnchannels(num_channels)
+        wavfile.setsampwidth(sample_width)
+        wavfile.setframerate(sample_rate)
+
+        for _ in range(periods):
+          wavfile.writeframes(data)
+
+        print('file written')
+
+    print('waiting for data ' + str(loop_counter))
+    time.sleep(1)
 
 def start_ble():
   global ble
@@ -106,77 +63,6 @@ def start_ble():
   time.sleep(3)
   run_app()
 
-def handle_connection():
-  global stop_app
-
-  if (ble.connected):
-    ble.disconnect = True
-    stop_app = True
-    connected_text.config(text='Disconnecting...')
-    connection_button.config(text='Disconnecting...')
-    time.sleep(2)
-    connected_text.config(text='Disconnected', fg="#D22B2B")
-    connection_button.config(text='Connect')
-  else:
-    ble.disconnect = False
-    stop_app = False
-    Thread(target=start_ble).start()
-
-# handle closing Monocle Dock
-def on_close():
-  global ble, stop_app
-
-  ble.disconnect = True # stops connection loop
-  stop_app = True
-  win.destroy()
-
-win = Tk()
-win.title('Monocle Dock')
-win.geometry("300x420")
-win.bg = 'white'
-win.iconbitmap(resource_path('monocle_24.ico'))
-
-canvas = Canvas(win, width=300, height=280, bg='white') # related height
-canvas.pack()
-
-img = Image.open(resource_path('monocle_200.jpg'))
-img = ImageTk.PhotoImage(img)
-canvas.create_image(50, 40, anchor=NW, image=img) # related height
-
-#Creates a Frame
-frame = LabelFrame(win, width=300, height=50, bd=1, text='Monocle status:', bg='#282828', fg='#0096FF')
-frame.pack(fill='both', expand='yes')
-frame.pack_propagate() # False keeps width
-
-# text
-connected_text = Label(frame, text='Connecting...', bg='#282828', fg='#AAFF00')
-connected_text.place(x=0, y=0)
-
-charging_text = Label(frame, text="Charging:", bg='#282828', fg='#AAFF00')
-charging_text.place(x=0, y=20)
-
-batt_level_text = Label(frame, text="Battery:", bg='#282828', fg='#AAFF00')
-batt_level_text.place(x=0, y=40)
-
-firmware_text = Label(frame, text="Firmware:", bg='#282828', fg='#AAFF00', anchor="e")
-firmware_text.place(x=150, y=0)
-
-ram_text = Label(frame, text="RAM:", bg='#282828', fg='#AAFF00', anchor="e")
-ram_text.place(x=150, y=20)
-
-storage_text = Label(frame, text="Storage:", bg='#282828', fg='#AAFF00', anchor="e")
-storage_text.place(x=150, y=40)
-
-uptime_text = Label(frame, text="Uptime:", bg='#282828', fg='#AAFF00', anchor="e")
-uptime_text.place(x=0, y=60)
-
-# manual connect/disconnect button
-connection_button = ttk.Button(frame, text= 'Disconnect' if ble and ble.connected else 'Connecting...', command=handle_connection)
-connection_button.place(x=110, y=85)
-
-Thread(target=start_ble).start()
-
-win.protocol("WM_DELETE_WINDOW", on_close)
-
-# render UI
-win.mainloop()
+# this loop is hard to terminate by ctrl+c
+# didn't add a way to stop the thread easily
+start_ble()
